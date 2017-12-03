95729a4dbcbe30e60d2c5a224e1dc8260c7d1e79
Habcio007 <33881111+Habcio007@users.noreply.github.com>
2017-12-03 23:28:59 +0100

    Add files via upload

--- filters_functions.c
+++ filters_functions.c
@@ -1,10 +1,255 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"structs.h"
-#include"filters_data.h"
 #include"filters_functions.h"
 
 Filters *matrix_filter;
+
+int AVERAGE_SIZE = 3;
+int AVERAGE[9] = { 1, 1, 1,
+1, 1, 1,
+1, 1, 1 };
+
+int SQUARE_SIZE = 5;
+int SQUARE[25] = { 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1,
+1, 1, 1, 1, 1,
+1, 1, 1, 1, 1,
+1, 1, 1, 1, 1 };
+
+int CIRCLE_SIZE = 5;
+int CIRCLE[25] = { 0, 1, 1, 1, 0,
+1, 1, 1, 1, 1,
+1, 1, 1, 1, 1,
+1, 1, 1, 1, 1,
+0, 1, 1, 1, 0 };
+
+int LP1_SIZE = 3;
+int LP1[9] = { 1, 1, 1,
+1, 2, 1,
+1, 1, 1 };
+
+int LP2_SIZE = 3;
+int LP2[9] = { 1, 1, 1,
+1, 4, 1,
+1, 1, 1 };
+
+int LP3_SIZE = 3;
+int LP3[9] = { 1, 1, 1,
+1, 12,1,
+1, 1, 1 };
+
+int PIRAMIDAL_SIZE = 5;
+int PIRAMIDAL[25] = { 1, 2, 3, 2, 1,
+2, 4, 6, 4, 2,
+3, 6, 9, 6, 3,
+2, 4, 6, 4, 2,
+1, 2, 3, 2, 1 };
+
+int CONUS_SIZE = 5;
+int CONUS[25] = { 0, 0, 1, 0, 0,
+0, 2, 2, 2, 0,
+1, 2, 5, 2, 1,
+0, 2, 2, 2, 0,
+0, 0, 1, 0, 0 };
+
+int GAUSS1_SIZE = 3;
+int GAUSS1[9] = { 1, 2, 1,
+2, 4, 2,
+1, 2, 1 };
+
+int GAUSS2_SIZE = 5;
+int GAUSS2[25] = { 1, 1, 2, 1, 1,
+1, 2, 4, 2, 1,
+2, 4, 8, 4, 2,
+1, 2, 4, 2, 1,
+1, 1, 2, 1, 1 };
+
+int GAUSS3_SIZE = 5;
+int GAUSS3[25] = { 0, 1, 2, 1, 0,
+1, 4, 8, 4, 1,
+2, 8, 16,8, 2,
+1, 4, 8, 4, 1,
+0, 1, 2, 1, 0 };
+
+int GAUSS4_SIZE = 5;
+int GAUSS4[25] = { 1, 4, 7, 4, 1,
+4,16,26,16, 4,
+7,26,41,26, 7,
+4,26,16,26, 4,
+1, 4, 7, 4, 1 };
+
+int GAUSS5_SIZE = 7;
+int GAUSS5[49] = { 1, 1, 2, 2, 2, 1, 1,
+1, 2, 2, 4, 2, 2, 1,
+2, 2, 4, 8, 4, 2, 2,
+2, 4, 8,16, 8, 4, 2,
+2, 2, 4, 8, 4, 2, 2,
+1, 2, 2, 4, 2, 2, 1,
+1, 1, 2, 2, 2, 1, 1 };
+
+int USUN_SREDNIA_SIZE = 3;
+int USUN_SREDNIA[9] = { -1, -1, -1,
+-1,  9, -1,
+-1, -1, -1 };
+
+int HP1_SIZE = 3;
+int HP1[9] = { 0, -1,  0,
+-1,  5, -1,
+0, -1,  0 };
+
+int HP2_SIZE = 3;
+int HP2[9] = { 1, -2,  1,
+-2,  5, -2,
+1, -2,  1 };
+
+int HP3_SIZE = 3;
+int HP3[9] = { 0, -1,  0,
+-1,  20, -1,
+0, -1,  0 };
+
+int HORIZONTAL_SIZE = 3;
+int POZIOMY[9] = { 0,  0,  0,
+-1,  1,  0,
+0,  0,  0 };
+
+int VERTICAL_SIZE = 3;
+int PIONOWY[9] = { 0, -1,  0,
+0,  1,  0,
+0,  0,  0 };
+
+int GRADIENT_E_SIZE = 3;
+int GRADIENT_E[9] = { -1,  1,  1,
+-1, -2,  1,
+-1,  1,  1 };
+
+int GRADIENT_SE_SIZE = 3;
+int GRADIENT_SE[9] = { -1, -1,  1,
+-1, -2,  1,
+1,  1,  1 };
+
+int GRADIENT_S_SIZE = 3;
+int GRADIENT_S[9] = { -1, -1, -1,
+1, -2,  1,
+1,  1,  1 };
+
+int GRADIENT_SW_SIZE = 3;
+int GRADIENT_SW[9] = { 1, -1, -1,
+1, -2, -1,
+1,  1,  1 };
+
+int GRADIENT_W_SIZE = 3;
+int GRADIENT_W[9] = { 1,  1, -1,
+1, -2, -1,
+1,  1, -1 };
+
+int GRADIENT_NW_SIZE = 3;
+int GRADIENT_NW[9] = { 1,  1,  1,
+1, -2, -1,
+1, -1, -1 };
+
+int GRADIENT_N_SIZE = 3;
+int GRADIENT_N[9] = { 1,  1,  1,
+1, -2,  1,
+-1, -1, -1 };
+
+int GRADIENT_NE_SIZE = 3;
+int GRADIENT_NE[9] = { 1,  1,  1,
+-1, -2,  1,
+-1, -1,  1 };
+
+int UWYPUKLAJACY_E_SIZE = 3;
+int UWYPUKLAJACY_E[9] = { -1,  0,  1,
+-1,  1,  1,
+-1,  0,  1 };
+
+int UWYPUKLAJACY_SE_SIZE = 3;
+int UWYPUKLAJACY_SE[9] = { -1, -1,  0,
+-1,  1,  1,
+0,  1,  1 };
+
+int UWYPUKLAJACY_S_SIZE = 3;
+int UWYPUKLAJACY_S[9] = { -1, -1, -1,
+0,  1,  0,
+1,  1,  1 };
+
+int UWYPUKLAJACY_SW_SIZE = 3;
+int UWYPUKLAJACY_SW[9] = { 0, -1, -1,
+1,  1, -1,
+1,  1,  0 };
+
+int UWYPUKLAJACY_W_SIZE = 3;
+int UWYPUKLAJACY_W[9] = { 1,  0, -1,
+1,  1, -1,
+1,  0, -1 };
+
+int UWYPUKLAJACY_NW_SIZE = 3;
+int UWYPUKLAJACY_NW[9] = { 1,  1,  0,
+1,  1, -1,
+0, -1, -1 };
+
+int UWYPUKLAJACY_N_SIZE = 3;
+int UWYPUKLAJACY_N[9] = { 1,  1,  1,
+0,  1,  0,
+-1, -1, -1 };
+
+int UWYPUKLAJACY_NE_SIZE = 3;
+int UWYPUKLAJACY_NE[9] = { 0,  1,  1,
+-1,  1,  1,
+-1, -1,  0 };
+
+int LAPL1_SIZE = 3;
+int LAPL1[9] = { 0, -1,  0,
+-1,  4, -1,
+0, -1,  0 };
+
+int LAPL2_SIZE = 3;
+int LAPL2[9] = { -1, -1, -1,
+-1,  8, -1,
+-1, -1, -1 };
+
+int LAPL3_SIZE = 3;
+int LAPL3[9] = { 1, -2,  1,
+-2,  4, -2,
+1, -2,  1 };
+
+int LAPL_SKOSNY_SIZE = 3;
+int LAPL_SKOSNY[9] = { -1,  0, -1,
+0,  4,  0,
+-1,  0, -1 };
+
+int LAPL_PIONOWY_SIZE = 3;
+int LAPL_PIONOWY[9] = { 0, -1,  0,
+0,  2,  0,
+0, -1,  0 };
+
+int LAPL_POZIOMY_SIZE = 3;
+int LAPL_POZIOMY[9] = { 0,  0,  0,
+-1,  2, -1,
+0,  0,  0 };
+
+int SOBEL_POZIOMY_SIZE = 3;
+int SOBEL_POZIOMY[9] = { 1,  2,  1,
+0,  0,  0,
+-1, -2, -1 };
+
+int SOBEL_PIONOWY_SIZE = 3;
+int SOBEL_PIONOWY[9] = { 1,  0, -1,
+2,  0, -2,
+1,  0, -1 };
+
+int PREWITT_POZIOMY_SIZE = 3;
+int PREWITT_POZIOMY[9] = { -1, -1, -1,
+0,  0,  0,
+1,  1,  1 };
+
+int PREWITT_PIONOWY_SIZE = 3;
+int PREWITT_PIONOWY[9] = { 1,  0, -1,
+1,  0, -1,
+1,  0, -1 };
+
+
+
 
 Filters *averagre_filter() {
 	Filters *filter;
@@ -287,6 +532,7 @@ Filters *clean_f(Filters *filter) {
 		free(filter->type);
 		return filter;
 }
+
 
 
 
